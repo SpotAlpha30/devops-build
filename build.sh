ec32b4e79ecf432aaecbd13b23eb129bf94e3e8c
SpotAlpha30 <138504569+SpotAlpha30@users.noreply.github.com>
2025-05-15 13:31:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# Build Docker image
-IMAGE_NAME=$1
-TAG=$2
-if [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
-echo "Usage: $0 <image_name> <tag>"
-exit 1
-fi
+
+IMAGE_NAME="sudharsan30/dev"
+TAG="latest"
 
 echo "Building image: $IMAGE_NAME:$TAG"
-docker build -t "$IMAGE_NAME:$TAG" .
-if [ $? -ne 0 ]; then
-echo "Failed to build $IMAGE_NAME:$TAG. Exiting."
-exit 1
-fi
+docker build -t $IMAGE_NAME:$TAG .
 
-echo "Build complete."
+echo "Logging in to DockerHub..."
+echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
+
+echo "Pushing image to DockerHub..."
+docker push $IMAGE_NAME:$TAG
